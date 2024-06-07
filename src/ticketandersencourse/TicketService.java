e8e37fe79930e4c47214f5a87d1f83522c2e8aa8
RodionovNikita <radzivonaumikita@gmail.com>
2024-06-07 17:19:22 +0200

    JFB-2: edit TicketService class
    created ticket storage

--- src/ticketandersencourse/TicketService.java
+++ src/ticketandersencourse/TicketService.java
@@ -1,19 +1,13 @@
 package ticketandersencourse;
 
-import java.math.BigDecimal;
-import java.time.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TicketService {
-    public static void main(String[] args) {
-        Ticket empty = new Ticket();
-        System.out.println(empty);
-        Ticket full = new Ticket("8965", "Elphi",
-                120, Instant.now().getEpochSecond(), true,
-                StadiumSector.B, 12.0, new BigDecimal("45"));
-        System.out.println(full);
-        Ticket limited = new Ticket("Filadelfia", 455, Instant.now().getEpochSecond());
-        System.out.println(limited);
 
+    List<Ticket> tickets;
 
+    public TicketService() {
+        tickets = new ArrayList<>();
     }
 }
