20589bd12d9d4e579d8decd4ecf2a31ae96bfc50
Dana <Dana.ackerman0914@gmail.com>
2024-06-22 22:44:55 +0500

    in main method declared Entity abstract class and instantiated with its derived class Ticket, and set id to its entity variable. Declared Roleprint, Printable, Shareable  interface and instantiated with Client class, printed overriden  methods from Client and Admin classes to demonstrate runtime polymorphism.

--- src/ticketandersencourse/service/TicketService.java
+++ src/ticketandersencourse/service/TicketService.java
@@ -1,13 +1,12 @@
 package ticketandersencourse.service;
 
-import ticketandersencourse.enums.Role;
-import ticketandersencourse.enums.StadiumSector;
 import ticketandersencourse.interfaces.Printable;
+import ticketandersencourse.interfaces.RolePrint;
 import ticketandersencourse.interfaces.Shareable;
 import ticketandersencourse.model.Ticket;
 import ticketandersencourse.user.Admin;
 import ticketandersencourse.user.Client;
-import ticketandersencourse.user.User;
+import ticketandersencourse.user.Entity;
 
 import java.math.BigDecimal;
 import java.time.Instant;
@@ -21,8 +20,8 @@ public class TicketService {
         ticketList = new ArrayList<>();
     }
 
-    public static List<Ticket> getTicketList() {
-        return ticketList;
+    public static void getTicketList() {
+        System.out.println(ticketList);
     }
 
     public static void setTicketList(List<Ticket> ticketList) {
@@ -38,55 +37,67 @@ public class TicketService {
         return null;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
         List<Ticket> tickets = new ArrayList<>();
-        tickets.add(new Ticket("4893", "hall #1", 489, Instant.now().getEpochSecond(), true, StadiumSector.A, 3.5, new BigDecimal("120")));
-        tickets.add(new Ticket("8624", "hall #2", 239, Instant.now().getEpochSecond(), true, StadiumSector.B, 4.0, new BigDecimal("150")));
-        tickets.add(new Ticket("9247", "hall #3", 769, Instant.now().getEpochSecond(), true, StadiumSector.C, 5.0, new BigDecimal("145")));
-        tickets.add(new Ticket("3581", "hall #4", 196, Instant.now().getEpochSecond(), true,  StadiumSector.B, 4.5, new BigDecimal("110")));
-        tickets.add(new Ticket("6384", "hall #5", 374, Instant.now().getEpochSecond(), true, StadiumSector.A, 3.0, new BigDecimal("125")));
-        tickets.add(new Ticket("4925", "hall #6", 815, Instant.now().getEpochSecond(), true, StadiumSector.A, 2.5, new BigDecimal("180")));
-        tickets.add(new Ticket("3702", "hall #7", 684, Instant.now().getEpochSecond(), true, StadiumSector.B, 2.0, new BigDecimal("85")));
-        tickets.add(new Ticket("4305", "hall #8", 106, Instant.now().getEpochSecond(), true, StadiumSector.C, 3.5, new BigDecimal("95")));
-        tickets.add(new Ticket("7900", "hall #9", 674, Instant.now().getEpochSecond(), true, StadiumSector.B, 5.5, new BigDecimal("165")));
-        tickets.add(new Ticket("1385", "hall #10", 285, Instant.now().getEpochSecond(), true, StadiumSector.A, 6.0, new BigDecimal("175")));
+        tickets.add(new Ticket("4893", "hall #1", 489, Instant.now().getEpochSecond(), true, 'A', 3.5, new BigDecimal("120"), 4852));
+        tickets.add(new Ticket("8624", "hall #2", 239, Instant.now().getEpochSecond(), true,'B', 4.0, new BigDecimal("150"), 3585));
+        tickets.add(new Ticket("9247", "hall #3", 769, Instant.now().getEpochSecond(), true, 'C', 5.0, new BigDecimal("145"), 1285));
+        tickets.add(new Ticket("3581", "hall #4", 196, Instant.now().getEpochSecond(), true,  'C', 4.5, new BigDecimal("110"), 8632));
+        tickets.add(new Ticket("6384", "hall #5", 374, Instant.now().getEpochSecond(), true,'A', 3.0, new BigDecimal("125"), 4485));
+        tickets.add(new Ticket("4925", "hall #6", 815, Instant.now().getEpochSecond(), true, 'B', 2.5, new BigDecimal("180"), 7963));
+        tickets.add(new Ticket("3702", "hall #7", 684, Instant.now().getEpochSecond(), true, 'B', 2.0, new BigDecimal("85"), 5896));
+        tickets.add(new Ticket("4305", "hall #8", 106, Instant.now().getEpochSecond(), true, 'C', 3.5, new BigDecimal("95"), 4586));
+        tickets.add(new Ticket("7900", "hall #9", 674, Instant.now().getEpochSecond(), true, 'A', 5.5, new BigDecimal("165"), 3658));
+        tickets.add(new Ticket("1385", "hall #10", 285, Instant.now().getEpochSecond(), true, 'A', 6.0, new BigDecimal("175"), 1486));
         setTicketList(tickets);
+        //getTicketList();
 
+
+        //empty
         Ticket empty = new Ticket();
-        //System.out.println(empty);
-        Ticket full = new Ticket("8965", "Elphi",
-                120, Instant.now().getEpochSecond(), true,
-                StadiumSector.B, 12.0, new BigDecimal("45"));
-        //System.out.println(full);
+        System.out.println(empty);
+        //full
+        Ticket full = new Ticket("4575", "Elphi",
+                    120, Instant.now().getEpochSecond(), true,
+                'B', 12.0, new BigDecimal("45"), 8524);
+        System.out.println(full);
+
+        //limited
         Ticket limited = new Ticket("Filadelfia", 455, Instant.now().getEpochSecond());
-        //System.out.println(limited);
+        System.out.println(limited);
 
-        //System.out.println(getTicketById("4893"));
-
-
-        Ticket ticket1 = new Ticket();
-        ticket1.getTicketValues(tickets);
+        //calling method by id
+        System.out.println(getTicketById("4893"));
 
 
-        Shareable shareable = new Ticket();
+        //hw 4
+
+        // method to get values of ticket fields
+        full.getTicketValues(tickets);
+
+
+
+        Entity entity = new Ticket();
+        entity.setId(1256);
+        System.out.println(entity.getId()); //getting id from its parent abstract class
+
+        RolePrint rolePrint1 = new Client();  // runtime polymorphism
+        System.out.println(rolePrint1.printRole());
+        RolePrint rolePrint2 = new Admin();   // runtime polymorphism
+        System.out.println(rolePrint2.printRole());
+
+
+        Printable printable1 = new Client();      // runtime polymorphism
+        printable1.print();
+        Printable printable2 = new Admin();        // runtime polymorphism
+        printable2.print();
+
+        Shareable shareable = new Ticket();             // runtime polymorphism
         shareable.sharedByEmail("JohnDoe@gmail.com");
-        shareable.sharedByPhone("+1126295978");
+        shareable.sharedByPhone("+112458962487");
 
 
 
-        User client = new Client(8625);
-        User admin = new Admin(1296);
-        System.out.println(client.printRole());
-        System.out.println(admin.printRole());
-        client.print();
-        admin.print();
-
-
-
-        Client clients = new Client();
-        clients.getTicket(full);
-        Admin administrator = new Admin();
-        administrator.checkTicket(full);
 
 
 
